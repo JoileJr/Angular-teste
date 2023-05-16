9436111b10621c86bec6f2eff3c5c64915b25e25
JoileJr <joilejunior2@gmail.com>
2023-05-16 10:09:42 -0300

    criando service e listando os cursos

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Course } from '../model/course';
+import { CoursesService } from '../services/courses.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-courses',
@@ -7,12 +9,11 @@ import { Course } from '../model/course';
   styleUrls: ['./courses.component.scss']
 })
 export class CoursesComponent {
-  courses: Course[] = [
-    {_id: "1", name: "angular", category: "front-end"}
-  ];
+  courses: Observable<Course[]>;
   displayedColumns = ['name', 'category'];
 
-  constructor(){
-
+  constructor(private coursesService: CoursesService){
+    this.courses = this.coursesService.list();
   }
+
 }
