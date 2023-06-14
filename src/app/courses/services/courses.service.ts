fc82788117237ac2769172f67e37b66e26abb4b8
JoileJr <joilejunior2@gmail.com>
2023-06-14 08:10:10 -0300

    spinner

--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Course } from '../model/course';
 import { HttpClient } from '@angular/common/http'
-import { tap } from 'rxjs';
+import { delay, first, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -13,6 +13,8 @@ export class CoursesService {
 
   list() {
     return this.httpClient.get<Course[]>(this.API).pipe(
+      first(),
+      delay(500),
       tap(courses => console.log(courses))
     );
   }
